0d8b61b9966a2c35fb3f39a6d8417087b75aceba
Iamlegend-Imani <imani.kirika116@gmail.com>
2021-05-03 19:05:10 -0400

    'addfiles'

--- Assignment1/__init__.py
+++ Assignment1/__init__.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import random
+from sklearn.model_selection import train_test_split
 
 def null_count(df):
     """This functions returns the number of null values in a Dataframe"""
@@ -25,25 +26,25 @@ This function should also take a random seed for reproducible randomization."""
 
 """The below functions, I am yet to work on
 
-def addy_split(addy_series)
-"""Split addresses into three columns (df['city'], df['state'], and df['zip']) - 
-you can use regexes to detect the format and pull out important pieces."""
+# def addy_split(addy_series)
+# Split addresses into three columns (df['city'], df['state'], and df['zip']) - 
+# you can use regexes to detect the format and pull out important pieces."""
 
-def abbr_2_st(state_series, abbr_2_st=True):
-"""Return a new column with the full name from a State abbreviation column -> 
-An input of FL would return Florida. 
-This function should also take a boolean (abbr_2_state) and 
-when False takes full state names and return state abbreviations. -> 
-An input of Florida would return Fl."""
+# def abbr_2_st(state_series, abbr_2_st=True):
+# """Return a new column with the full name from a State abbreviation column -> 
+# An input of FL would return Florida. 
+# This function should also take a boolean (abbr_2_state) and 
+# when False takes full state names and return state abbreviations. -> 
+# An input of Florida would return Fl."""
 
-def list_2_series(list_2_series, df):
-"""Single function to take a list and dataframe then turn the list into a series and 
-add it to the inputted dataframe as a new column."""
+# def list_2_series(list_2_series, df):
+# """Single function to take a list and dataframe then turn the list into a series and 
+# add it to the inputted dataframe as a new column."""
 
-def rm_outlier(df):
-"""A 1.5*Interquartile range outlier detection/removal function that gets rid of outlying rows and 
-returns that outlier cleaned dataframe."""
+# def rm_outlier(df):
+# """A 1.5*Interquartile range outlier detection/removal function that gets rid of outlying rows and 
+# returns that outlier cleaned dataframe."""
 
-def split_dates(date_series):
-"""Function to split dates of format "MM/DD/YYYY" into multiple columns (df['month'], df['day'], df['year']) 
-and then return the same dataframe with those additional columns."""
+# def split_dates(date_series):
+# """Function to split dates of format "MM/DD/YYYY" into multiple columns (df['month'], df['day'], df['year']) 
+# and then return the same dataframe with those additional columns."""
