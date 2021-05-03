d1b9f66738a19e37b3e7d00b0b1507e9a45189c8
Iamlegend-Imani <imani.kirika116@gmail.com>
2021-05-03 17:21:02 -0400

    'add'

--- LamData/__init__.py
+++ LamData/__init__.py
@@ -1,1 +1,10 @@
- 
+ """LambData - A collection of Data Science helper functions"""
+
+ import pandas as pd
+ import numpy as np
+
+ FAVORITES_ANIMALS = ["Lion","Elephant","Gorilla","Cheetah","Ostrich"]
+
+ def null_count(df):
+     """Cleans Pandas DataFrame"""
+     # ToDO - implement
